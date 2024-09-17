fa356fea8a8b9f3c98f848fd8e5df7e7201b4eab
Ronald Torres <rtorres5@atu.edu>
2024-09-17 10:50:58 -0500

    Finished with login just need to add animations

---
+++ lib/login_page.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ) //MaterialApp
+);
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      key: const Key('homePageScaffold'), //Added key
+      body: Container(
+        key: const Key('gradientContainer'), //Added Key
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.green.shade900,
+                  Colors.green.shade600,
+                  Colors.green.shade300
+                ]
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 80,),
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget> [
+                  Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
+                  SizedBox(height: 10,),
+                  Text("Welcome Back", style: TextStyle(color: Colors.white,fontSize: 18),),
+                ],
+              ),
+            )
+            ,Expanded(
+              key: const Key('expandedContainer'), //Added key
+              child:Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60)),
+                ),
+                child: Padding(
+                  key: const Key('contentPadding'), //Added key
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 60,),
+                      Container(
+                        padding: const EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [BoxShadow(
+                              color: Color.fromRGBO(225, 95, 27, .3),
+                              blurRadius: 20,
+                              offset: Offset(0,10)
+                          ),],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.grey[200]!)),
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Email or Phone number",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.grey[200]!)),
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 40,),
+                      const Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
+                      const SizedBox(height: 40,),
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.greenAccent
+                        ),
+                        child: const Center(
+                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      const SizedBox(height: 50,),
+                      const Text("Continue with social media", style: TextStyle(color: Colors.grey),),
+                      const SizedBox(height: 30,),
+                      const SizedBox(height:80,),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child:
+                            Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.blue
+                              ),
+                              child: const Center(
+                                child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 30,),
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.black
+                              ),
+                              child: const Center(
+                                child: Text("Github", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

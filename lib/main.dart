7f62b027f54c924bc353cf3baec844ea22797297
Ronald Torres <rtorres5@atu.edu>
2024-09-17 11:24:15 -0500

    Finished with login just need to add animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:buildingapp/login_page.dart';  //imports login page
+import 'login_page.dart'; // Imports login page
+
 void main() {
   runApp(const MyApp());
 }
-//Let's go
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Parking App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Test Parking App Home Page'),
+      home: HomePage(), // Set HomePage from login_page.dart as the initial screen
     );
   }
 }
 
+// Keep the MyHomePage class for navigation purposes
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -40,23 +42,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have hurt this button this many times:',
+              'You have pressed this button this many times:',
             ),
             Text(
               '$_counter',
@@ -69,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }

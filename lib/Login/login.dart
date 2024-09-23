55ba0459037f937cb99b1afd05a366bd29694cb1
Ronald Torres <rtorres5@atu.edu>
2024-09-23 13:01:10 -0500

    Login Changes

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -1,1 +1,78 @@
 //Future login page
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState()=> _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  //Text editors for when text inputed
+  final username = TextEditingController();
+  final password = TextEditingController();
+
+  //Bool to show and hide password
+  bool isVisible =false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+
+
+              //Username field
+              Container(
+                margin: EdgeInsets.all(8),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+            color: Colors.lightGreen.withOpacity(.3)),
+                child: TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  border: InputBorder.none,
+                  hintText: "Username",
+                ),
+              ),
+              ),
+
+              //Password field
+              Container(
+                margin: EdgeInsets.all(8),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.lightGreen.withOpacity(.3)),
+                child: TextFormField(
+                  obscureText: !isVisible,
+                  decoration:  InputDecoration(
+                    icon: Icon(Icons.lock),
+                    border: InputBorder.none,
+                    hintText: "Password",
+                    suffixIcon: IconButton(onPressed: () {
+                      //Hide and show
+                      setState(() {
+                        //toggle button
+                        isVisible= !isVisible;
+                      });
+                    },
+                        icon: Icon(isVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off))),
+                  ),
+                ),
+              ),
+            ],
+          ),),
+        ),
+      ),
+    )
+  }
+}
--- lib/Login/sign_up.dart
+++ lib/Login/sign_up.dart
@@ -1,2 +1,24 @@
 import 'package:flutter/material.dart';
 
+class SignUp extends StatefulWidget{
+  const SignUp({super.key})
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignupState extends State<SignUp>{
+  final username= TextEditingController();
+  final password= TextEditingController();
+  final confirmPassword= TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+      body: Column(
+      children: [
+
+
+      ],
+    )
+}
+}

074b7f181d3d97c4df8475c5374c431459fd3406
Ronald Torres <rtorres5@atu.edu>
2024-10-02 17:12:39 -0500

    Google Map changes

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import "package:flutter/material.dart";
+
+const String google_api_key ="AIzaSyBUEzTjOAYCGAG3S98BjXjOJq5zAsNmJOA";
+const Color primaryColor= Colors.redAccent;
+const double defaultPadding=16.0;
